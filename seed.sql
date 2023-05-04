87af253963214fdc69c0b0ecc95329b0da950ac8
Malek Al-Sheekh Hasan <malek.alsheekh01@gmail.com>
2023-05-04 21:07:09 +0000

    Lab 15 is done

---
+++ seed.sql
@@ -0,0 +1,5 @@
+INSERT INTO movie (title,runtime,gener,actors,plot,imdbrating) values ('fast furios','150 minute','action,drama','malek','malek','18');
+INSERT INTO movie (title,runtime,gener,actors,plot,imdbrating) values ('fast furios','150 minute','action,drama','ahmad','ahmad','18');
+INSERT INTO movie (title,runtime,gener,actors,plot,imdbrating) values ('fast furios','150 minute','action,drama','murad','murad','18');
+INSERT INTO movie (title,runtime,gener,actors,plot,imdbrating) values ('fast furios','150 minute','action,drama','loay','loay','18');
+INSERT INTO movie (title,runtime,gener,actors,plot,imdbrating) values ('fast furios','150 minute','action,drama','malek','malek','18');
